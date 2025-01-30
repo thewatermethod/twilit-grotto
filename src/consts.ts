46d8a939ebd9464633f2b97417d7281b7079c619
Matt Bevilacqua <thewatermethod@gmail.com>
2025-01-30 15:25:34 -0500

    Refactor grids to better accomodate multiple products

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Twilit Grotto Press";
-export const SITE_DESCRIPTION = "";
+export const SITE_DESCRIPTION =
+  "Twilit Grotto is a small, independent publisher of material for old-school roleplaying games and zines.";
